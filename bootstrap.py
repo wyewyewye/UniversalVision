32110ef13e9b649fac706688e23a40106e792e6c
wyewyewye <1083183486@qq.com>
2025-04-15 00:46:08 +0800

    [add]: add cmake configure result message

--- bootstrap.py
+++ bootstrap.py
@@ -15,3 +15,10 @@ if not os.path.exists(CMakeConfig.BUILD_DIR):
     print(f"Create build dir: {CMakeConfig.BUILD_DIR}")
     
 p = subprocess.Popen("cmake .. -G \"Visual Studio 17 2022\" " + "".join(cmake_params), cwd=CMakeConfig.BUILD_DIR, shell=True)
+
+p.wait()
+
+if p.returncode == 0:
+    print("CMake configure success!")
+else:
+    print("CMake configure failed!")

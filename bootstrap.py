c4f21009fe2b493bd1440ab26195f0154584fe23
wyewyewye <1083183486@qq.com>
2025-04-15 00:45:01 +0800

    [add]: add bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,1 +1,17 @@
 import os
+import subprocess
+from config import *
+
+CMakeConfig.BUILD_DIR = os.path.abspath(CMakeConfig.BUILD_DIR)
+CMAKE_PARAMS = {key: value for key, value in CMakeConfig.__dict__.items() if not key.startswith("__")}
+cmake_params = []
+
+for k, v in CMAKE_PARAMS.items():
+    print(f"{k}: {v}")
+    cmake_params.append(f"-D{k}:STRING={v} ")
+
+if not os.path.exists(CMakeConfig.BUILD_DIR):
+    os.makedirs(CMakeConfig.BUILD_DIR)
+    print(f"Create build dir: {CMakeConfig.BUILD_DIR}")
+    
+p = subprocess.Popen("cmake .. -G \"Visual Studio 17 2022\" " + "".join(cmake_params), cwd=CMakeConfig.BUILD_DIR, shell=True)
---
+++ config.py
@@ -0,0 +1,6 @@
+class CMakeConfig:
+    BUILD_DIR = "build"
+
+    GLFW_INCLUDE_PATH = r'D:\workspace\cpp_project\glfw-3.4.bin.WIN64\include'
+
+    GLFW_LIB_PATH = r'D:\workspace\cpp_project\glfw-3.4.bin.WIN64\lib-vc2022'

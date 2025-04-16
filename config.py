78efca7e61085a84efea00aa759bdc14f59369d6
伍衍恩 <wuyanen@mindlinker.com>
2025-04-16 15:59:20 +0800

    [add]: add glfw download link

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class CMakeConfig:
     BUILD_DIR = "build"
 
+    # https://www.glfw.org/download.html
     GLFW_INCLUDE_PATH = r'D:\workspace\cpp_project\glfw-3.4.bin.WIN64\include'
 
     GLFW_LIB_PATH = r'D:\workspace\cpp_project\glfw-3.4.bin.WIN64\lib-vc2022'
